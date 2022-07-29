92658558db1a006bf5ebee402e81cbcddf1e0a31
Murphy <tnm12@NIST.GOV>
2022-07-29 16:06:20 -0400

    Added Sequence Compiler

--- ctRSD-simulator-2.0/Simulatorv2021.py
+++ ctRSD-simulator-2.0/Simulatorv2021.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 
@@ -64,6 +65,9 @@ from matplotlib.ticker import MaxNLocator
 import re
 import time
 import math
+import xlrd
+xlrd.xlsx.ensure_elementtree_imported(False, None)
+xlrd.xlsx.Element_has_iter = True
 
 
 def rate_eqs(t,y,ktxnO,ktxnG,ktxnTh,ktxnF,ktxnAG,ktxnCG,krz,krsd,krev,krep,krepr,kth,krzTh,krsdF,krevF,krevA,krsdA,krzA,krevCG,krsdCG,krzCG,leak,leakA,Otempm,Gtempm,Thtempv,Ftempv,AGtempm,AGmap,CGtempm,CGmap,kssdO,kssdF,kdsduG,kdsdG,kdsdGO,kdsduAG,kdsdAG,kdsduCG,kdsdCG,kdrd,khybO,khybR,N):
@@ -223,6 +227,60 @@ def rate_eqs(t,y,ktxnO,ktxnG,ktxnTh,ktxnF,ktxnAG,ktxnCG,krz,krsd,krev,krep,krepr
     
     return(np.concatenate([duGm,dGm,dOm,dGOm,dRv,dSv,dROm,duThv,dThv,dFv,dGFv,duAGm,dAGm,dAGOam,dAGObm,duCGm,dCGm,dCGOam,dCGObm,dAGFbv,dQv]))
 
+
+def rc_seq(seq, spec_out='rc', rna=0):
+
+    new_seq = '3'+seq+'5'
+    new_seq = list(new_seq)
+    seq = list(seq)
+    ds = ''
+
+    for i in range(len(seq)):
+        if seq[i].lower() == 'u' and rna == 0:
+            seq[i] = 'T'
+        elif seq[i].lower() == 't' and rna == 1:
+            seq[i] = 'u'
+        if seq[i].lower() == 'g':
+            new_seq[i+1] = 'C'
+        elif seq[i].lower() == 'c':
+            new_seq[i+1] = 'G'
+        elif seq[i].lower() == 't':
+            new_seq[i+1] = 'A'
+        elif seq[i].lower() == 'u':
+            new_seq[i+1] = 'A'
+        elif seq[i].lower() == 'a' and rna == 0:
+            new_seq[i+1] = 'T'
+        elif seq[i].lower() == 'a' and rna == 1:
+            new_seq[i+1] = 'U'
+
+    if spec_out.lower() == 'rc':
+        out_seq = ds.join(new_seq[::-1])
+    elif spec_out.lower() == 'c':
+        out_seq = ds.join(new_seq)
+    elif spec_out.lower() == 'r':
+        out_seq = '3'+ds.join(seq[::-1])+'5'
+    elif spec_out.lower() == 's':
+        out_seq = '5'+ds.join(seq[:])+'3'
+
+    return out_seq
+
+
+def xlsheet_to_dict(filepath, sheet_name, ncol):
+
+    seq_workbook = xlrd.open_workbook(filepath)
+    sheet = seq_workbook.sheet_by_name(sheet_name)
+    sheet_dict = {}
+    dv = [[0]*ncol for i in range(0, sheet.nrows)]
+    for ri in range(1, sheet.nrows):
+        dk = sheet.cell_value(ri, 0)
+        a = 0
+        for ci in range(0, ncol):
+            dv[ri][a] = sheet.cell_value(ri, ci+1)
+            a += 1
+
+        sheet_dict.update({dk: dv[ri]})
+
+    return sheet_dict
 
 class RSD_sim:
     def __init__(self,domains=5):
@@ -465,9 +523,9 @@ class RSD_sim:
         S =  re.compile("s\{\w*\d+\w*\}")
         Ss = S.fullmatch(name.lower())
         
-        uTh = re.compile("utg/ut/uth\{\w*\d+\w*\}")
+        uTh = re.compile("utg|ut|uth\{\w*\d+\w*\}")
         uThs = uTh.fullmatch(name.lower())
-        thresh = re.compile("tg/t/th\{\w*\d+\w*\}")
+        thresh = re.compile("(tg|t|th)\{\w*\d+\w*\}")
         threshs = thresh.fullmatch(name.lower())
         
         fuel = re.compile("f\{\w*\d+\w*\}")
@@ -1364,8 +1422,498 @@ class RSD_sim:
         
         
 
+    def ctRSD_seq_compile(self,name,filepath,Rz='Ro',L='L',term='T7t',hp5='5hp',eI='',eO='',s='',invert=0):
+        
+        th_d = xlsheet_to_dict(filepath, 'th', 1)  # toehold sequences
+        # input branch migration seqeuences
+        I_BM_d = xlsheet_to_dict(filepath, 'I BM', 1)
+        # output branch migration seqeuences
+        O_BM_d = xlsheet_to_dict(filepath, 'O BM', 1)
+        r_d = xlsheet_to_dict(filepath, 'r', 1)  # reversible domain on reporters
+        L_d = xlsheet_to_dict(filepath, 'L', 1)  # Linker for ribozyme
+        # extended branch migration domain
+        e_d = xlsheet_to_dict(filepath, 'e', 1)
+        Rz_d = xlsheet_to_dict(filepath, 'Rz', 1)  # ribozymes
+        term_d = xlsheet_to_dict(filepath, 'term', 1)  # terminators
+        hp5_d = xlsheet_to_dict(filepath, '5hp', 1)  # 5' hairpin sequences
+        # sequences upstream of promoter
+        us_d = xlsheet_to_dict(filepath, 'US', 1)
+        # sequences downstream of terminator
+        ds_d = xlsheet_to_dict(filepath, 'DS', 1)
 
 
+        
+        i_th = ''
+        o_th = ''
+        i_bm = ''
+        o_bm = ''
+        
+        gate = re.compile("(g|gate)\{\w\d+(\-\d+)*e*\,\w\d+(\-\d+)*(r*|e*)\}")
+        gates = gate.fullmatch(name.lower())
+        
+        AG = re.compile("(ag|g|gate)\{\w\d+(\-\d+)*(\.|\&)\w\d+(\-\d+)*\,\w\d+(\-\d+)*r*\}")
+        AGs = AG.fullmatch(name.lower())
+        
+        inp = re.compile("(i|in|inp|input)\{\w\d+(\-\d+)*e*\}")
+        inps = inp.fullmatch(name.lower())
+        
+        thresh = re.compile("(tg|t|th)\{\w\d+(\-\d+)*(r*|e*)\}")
+        threshs = thresh.fullmatch(name.lower())
+        
+        fuel = re.compile("f\{\w\d+(\-\d+)*e*\}")
+        fuels = fuel.fullmatch(name.lower())
+        
+        CG = re.compile("cg\{\w\d+(\-\d+)*\,\w\d+(\-\d+)*\}")
+        CGs = CG.fullmatch(name.lower())
+        
+        
+        EcheckInp = re.compile("e\,")
+        EcheckInps = EcheckInp.search(name.lower())
+        
+        EcheckOut = re.compile("e\}")
+        EcheckOuts = EcheckOut.search(name.lower())
+        
+        if (eI == '' and EcheckInps) or (eO == '' and EcheckOuts):
+            print('e included in species name, but no domain number was provided in function declaration.')
+            return('Function terminated, see error.')
+            
+        
+        if gates:
+        
+            dashcheckInp = re.compile("\-\d+e*\,")
+            dashcheckInps = dashcheckInp.search(name.lower())
+            
+            dashcheckOut = re.compile("\-\d+(r*|e*)\}")
+            dashcheckOuts = dashcheckOut.search(name.lower())
+            
+            EcheckInp = re.compile("e\,")
+            EcheckInps = EcheckInp.search(name.lower())
+            
+            EcheckOut = re.compile("e\}")
+            EcheckOuts = EcheckOut.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+            
+            RcheckOut = re.compile("r\}")
+            RcheckOuts = RcheckOut.search(name.lower())
+            
+            
+            if dashcheckInps and dashcheckOuts:
+                i_th = Indf[0]
+                o_th = Indf[1]
+                i_bm = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+dashIndf[1][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[1][1:]
+                
+                
+            elif dashcheckInps:
+                i_th = Indf[0]
+                o_th = Indf[1][0]
+                i_bm = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+Indf[1][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[1][1:]
+                
+            elif dashcheckOuts:
+                i_th = Indf[0][0]
+                o_th = Indf[1]
+                i_bm = 'I'+Indf[0][1:]
+                o_bm = 'O'+dashIndf[0][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[0][1:]
+                    
+                
+            else:
+                i_th = Indf[0][0]
+                o_th = Indf[1][0]
+                i_bm = 'I'+Indf[0][1:]
+                o_bm = 'O'+Indf[1][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[1][1:]
+                    
+                    
+            if EcheckInps and EcheckOuts:
+                eI = 'e'+eI
+                eO = 'e'+eO
+                
+            
+            elif EcheckInps:
+                eI = 'e'+eI
+                
+            
+            elif EcheckOuts:
+                eO = 'e'+eO
+                
+                
+            
+            if RcheckOuts:
+                r_S = r_d[r_k][0]
+            else:
+                r_S = ''
+            o_bm_S = O_BM_d[o_bm][0]
+            o_th_Sp = rc_seq(th_d[o_th][0])[1:-1]
+            i_bm_S = I_BM_d[i_bm][0]
+            i_th_S = th_d[i_th][0]
+            i_bm_Sp = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            o_th_S = th_d[o_th][0]
+            
+                
+        
+            
+        elif AGs:
+            
+            dashcheckInp1 = re.compile("\-\d+(\.|\&)")
+            dashcheckInps1 = dashcheckInp1.search(name.lower())
+            dashcheckInp2 = re.compile("\-\d+\,")
+            dashcheckInps2 = dashcheckInp2.search(name.lower())
+            
+            dashcheckOut = re.compile("\-\d+r*\}")
+            dashcheckOuts = dashcheckOut.search(name.lower())
+            
+            RcheckOut = re.compile("r\}")
+            RcheckOuts = RcheckOut.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+        
+            
+            
+            if dashcheckInps1 and dashcheckInps2 and dashcheckOuts:
+                i_th1 = Indf[0]
+                i_th2 = Indf[1]
+                o_th = Indf[2]
+                i_bm1 = 'I'+dashIndf[0][1:]
+                i_bm2 = 'I'+dashIndf[1][1:]
+                o_bm = 'O'+dashIndf[2][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[2][1:]
+                
+                
+            elif dashcheckInps1 and dashcheckInps2:
+                i_th1 = Indf[0]
+                i_th2 = Indf[1]
+                o_th = Indf[2][0]
+                i_bm1 = 'I'+dashIndf[0][1:]
+                i_bm2 = 'I'+dashIndf[1][1:]
+                o_bm = 'O'+Indf[2][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[2][1:]
+                
+            elif dashcheckInps1 and dashcheckOuts:
+                i_th1 = Indf[0]
+                i_th2 = Indf[1][0]
+                o_th = Indf[2]
+                i_bm1 = 'I'+dashIndf[0][1:]
+                i_bm2 = 'I'+Indf[1][1:]
+                o_bm = 'O'+dashIndf[1][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[1][1:]
+                    
+            elif dashcheckInps2 and dashcheckOuts:
+                i_th1 = Indf[0][0]
+                i_th2 = Indf[1]
+                o_th = Indf[2]
+                i_bm1 = 'I'+Indf[0][1:]
+                i_bm2 = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+dashIndf[1][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[1][1:]
+            
+            elif dashcheckInps1:
+                i_th1 = Indf[0]
+                i_th2 = Indf[1][0]
+                o_th = Indf[2][0]
+                i_bm1 = 'I'+dashIndf[0][1:]
+                i_bm2 = 'I'+Indf[1][1:]
+                o_bm = 'O'+Indf[2][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[2][1:]
+                
+            elif dashcheckInps2:
+                i_th1 = Indf[0][0]
+                i_th2 = Indf[1]
+                o_th = Indf[2][0]
+                i_bm1 = 'I'+Indf[0][1:]
+                i_bm2 = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+Indf[2][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[2][1:]
+            
+            elif dashcheckOuts:
+                i_th1 = Indf[0][0]
+                i_th2 = Indf[1][0]
+                o_th = Indf[2]
+                i_bm1 = 'I'+Indf[0][1:]
+                i_bm2 = 'I'+Indf[1][1:]
+                o_bm = 'O'+dashIndf[0][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[0][1:]
+                           
+            else:
+                i_th1 = Indf[0][0]
+                i_th2 = Indf[1][0]
+                o_th = Indf[2][0]
+                i_bm1 = 'I'+Indf[0][1:]
+                i_bm2 = 'I'+Indf[1][1:]
+                o_bm = 'O'+Indf[2][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[2][1:]
+                    
+        
+            if RcheckOuts:
+                r_S = r_d[r_k][0]
+            else:
+                r_S = ''
+            o_bm_S = O_BM_d[o_bm][0]
+            o_th_Sp = rc_seq(th_d[o_th][0])[1:-1]
+            i_bm_S1 = I_BM_d[i_bm1][0]
+            i_bm_S2 = I_BM_d[i_bm2][0]
+            i_th_S1 = th_d[i_th1][0]
+            i_th_S2 = th_d[i_th2][0]
+            i_bm_Sp1 = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            i_bm_Sp2 = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            o_th_S = th_d[o_th][0]
+            
+                    
+                    
+        
+        elif inps:
+            
+            dashcheckOut = re.compile("\-\d+e*\}")
+            dashcheckOuts = dashcheckOut.search(name.lower())
+            
+            EcheckInp = re.compile("e\}")
+            EcheckInps = EcheckInp.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+            
+            
+                
+            if dashcheckOuts:
+                o_th = Indf[0]
+                o_bm = 'O'+dashIndf[0][1:]
+                    
+                
+            else:
+                o_th = Indf[0][0]
+                o_bm = 'O'+Indf[0][1:]
+                    
+                    
+            if EcheckInps:
+                eI = 'e'+eI
+                
+                
+            
+            o_bm_S = O_BM_d[o_bm][0]
+            o_th_Sp = rc_seq(th_d[o_th][0])[1:-1]
+            o_th_S = th_d[o_th][0]
+            i_bm_Sp = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            r_S = ''       
+            i_bm_S = ''
+            i_th_S = ''
+            i_bm_Sp = ''
+        
+            
+        
+        elif threshs:
+            
+            dashcheckInp = re.compile("\-\d+(r*|e*)\}")
+            dashcheckInps = dashcheckInp.search(name.lower())
+            
+            EcheckInp = re.compile("e\}")
+            EcheckInps = EcheckInp.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+            
+            RcheckOut = re.compile("r\}")
+            RcheckOuts = RcheckOut.search(name.lower())
+            
+            
+            
+            if dashcheckInps:
+                i_th = Indf[0]
+                i_bm = 'I'+dashIndf[0][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+dashIndf[0][1:]
+                    
+                
+            else:
+                i_th = Indf[0][0]
+                i_bm = 'I'+Indf[0][1:]
+                
+                if RcheckOuts:
+                    r_k = 'r'+Indf[0][1:]
+                    
+                      
+            if EcheckInps:
+                eI = 'e'+eI
+                
+                
+                
+            if RcheckOuts:
+                r_S = r_d[r_k][0]
+            else:
+                r_S = ''
+            i_bm_S = I_BM_d[i_bm][0]
+            i_th_S = th_d[i_th][0]
+            i_bm_Sp = ''
+            o_bm_S = ''
+            o_th_Sp = ''
+            o_th_S = ''
+                
+            
+            
+                
+        elif fuels:
+            
+            dashcheckOut = re.compile("\-\d+e*\}")
+            dashcheckOuts = dashcheckOut.search(name.lower())
+            
+            EcheckInp = re.compile("e\}")
+            EcheckInps = EcheckInp.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+            
+            
+                
+            if dashcheckOuts:
+                o_th = Indf[0]
+                o_bm = 'O'+dashIndf[0][1:]
+                    
+                
+            else:
+                o_th = Indf[0][0]
+                o_bm = 'O'+Indf[0][1:]
+                    
+                    
+            if EcheckInps:
+                eI = 'e'+eI
+                
+        
+            o_bm_S = O_BM_d[o_bm][0]
+            o_th_Sp = rc_seq(th_d[o_th][0])[1:-1]
+            o_th_S = th_d[o_th][0]
+            i_bm_Sp = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            r_S = ''
+            i_bm_S = ''
+            i_th_S = ''
+            
+            
+            
+                
+        elif CGs:
+            dashcheckInp = re.compile("\-\d+\,")
+            dashcheckInps = dashcheckInp.search(name.lower())
+            
+            dashcheckOut = re.compile("\-\d+\}")
+            dashcheckOuts = dashcheckOut.search(name.lower())
+            
+            Indf = re.findall("[a-z]\d+",name.lower())
+            dashIndf = re.findall("\-\d+",name.lower())
+            
+            
+            
+            if dashcheckInps and dashcheckOuts:
+                i_th = Indf[0]
+                o_th = Indf[1]
+                i_bm = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+dashIndf[1][1:]
+            
+                
+                
+            elif dashcheckInps:
+                i_th = Indf[0]
+                o_th = Indf[1][0]
+                i_bm = 'I'+dashIndf[0][1:]
+                o_bm = 'O'+Indf[1][1:]
+                
+        
+                
+            elif dashcheckOuts:
+                i_th = Indf[0][0]
+                o_th = Indf[1]
+                i_bm = 'I'+Indf[0][1:]
+                o_bm = 'O'+dashIndf[0][1:]
+            
+                    
+                
+            else:
+                i_th = Indf[0][0]
+                o_th = Indf[1][0]
+                i_bm = 'I'+Indf[0][1:]
+                o_bm = 'O'+Indf[1][1:]
+                
+                
+            
+            
+            o_bm_S = O_BM_d[o_bm][0]
+            o_th_Sp = rc_seq(th_d[o_th][0])[1:-1]
+            i_bm_S = I_BM_d[i_bm][0]
+            i_th_S = th_d[i_th][0]
+            i_bm_Sp = rc_seq(O_BM_d[o_bm][0])[1:-1]
+            o_th_S = th_d[o_th][0]
+            r_S = ''
+            
+                
+        
+           
+        else:
+            print('Incorrect Nomenclature')
+            
+        
+        
+        
+        #Extracted sequences
+        hp5_S = hp5_d[hp5][0]
+        l_S = L_d[L][0]
+        rz_S = Rz_d[Rz][0]
+        term_S = term_d[term][0]
+        ei_S = ''
+        ei_Sp = ''
+        eo_S = ''
+        s_S = ''
+        
+         
+        
+        G_u1_w2r = hp5_S +\
+            eo_S + r_S + o_bm_S + o_th_Sp + ei_S + i_bm_S +\
+            l_S + rz_S +\
+            s_S + i_th_S + i_bm_Sp + ei_Sp + o_th_S +\
+            term_S
+        
+        G_temp = 'TTCTAATACGACTCACTATA' + rc_seq(G_u1_w2r, spec_out='s')[1:-1]
+        
+        '''
+        
+        Gi_u1_w2r = hp5_S +\
+            s_S + i_th_S + i_bm_Sp + ei_Sp + o_th_S +\
+            l_S + rz_S +\
+            eo_S + r_S + o_bm_S + o_th_Sp + ei_S + i_bm_S +\
+            term_S
+        
+        Gi_temp = 'TTCTAATACGACTCACTATA' + rc_seq(Gi_u1_w2r, spec_out='s')[1:-1]
+        
+        '''
+        
+        return(G_temp) 
 
 
 

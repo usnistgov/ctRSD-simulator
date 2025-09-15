bd2fe78ed2bf859c29422e87a997e646d7ba7c05
Schaffter <sws5@NIST.GOV>
2025-09-15 14:03:33 -0400

    Changes

--- ctRSD-simulator-2.0/docs/conf.py
+++ ctRSD-simulator-2.0/docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('C:\\Users\\sws5\\OneDrive - National Institute of Standards and Technology (NIST)\\ctRSD simulator and documentation\\ctRSD-simulator\\simulatorenv\\Lib\\site-packages'))
 
 
 # -- Project information -----------------------------------------------------
